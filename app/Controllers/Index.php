f597fe6c1bc9e539d746335dbefa35ae70d2956a
PisekPiskovec <pisekpiskovec@gmail.com>
2025-06-19 00:59:49 +0200

    feat: Add database setup feature with route, method, and model.
    - Add new route for database setup.
    - Add method for database setup with exception handling and JSON response.
    - Create a new User model with defined database table and field configurations, including constraints like required, unique, and default values.

--- app/Controllers/Index.php
+++ app/Controllers/Index.php
@@ -2,10 +2,27 @@
 
 namespace Controllers;
 
+use Exception;
+
 class Index
 {
     public function getStatus(\Base $base)
     {
         JSON_response(['message' => 'API is running', 'status' => 'ok']);
+    }
+
+    public function getDBsetup(\Base $base)
+    {
+        try {
+            \Models\User::setdown();
+        } catch (Exception $e) {
+            JSON_response($e->getMessage(), $e->getMessage());
+        }
+        try {
+            \Models\User::setup();
+        } catch (Exception $e) {
+            JSON_response($e->getMessage(), $e->getMessage());
+        }
+        JSON_response(true);
     }
 }
