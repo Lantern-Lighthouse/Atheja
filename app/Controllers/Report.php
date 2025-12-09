2b14c2210a54e1abbadf4b90d74f5708ef1de6fa
Písek Pískovec <pisekpiskovec@gmail.com>
2025-12-09 22:29:48 +0100

    feat: Add report management features
    - Add new folder configuration file with specified properties.
    - Add new HTTP request model configuration with defined parameters.
    - Add GET route for report management to handle report retrieval.
    - Add Report controller to manage report data retrieval and user authorization checks.

---
+++ app/Controllers/Report.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Controllers;
+
+class Report
+{
+    public function getReport(\Base $base)
+    {
+        $rbac = \lib\RibbitCore::get_instance($base);
+        $user = VerifySessionToken($base);
+        $rbac->set_current_user($user);
+        if (!$rbac->has_role('moderator') && !$rbac->has_role('admin'))
+            return \lib\Responsivity::respond('Unauthorized', \lib\Responsivity::HTTP_Unauthorized);
+
+        $model = new \Models\Report();
+        $report = $model->findone(['id=?', $base->get('PARAMS.reportID')]);
+        unset($model);
+
+        $response = [
+            'reporter' => $report->reporter->username,
+            ...(isset($report->user_reported) ? ['reported' => $report->user_reported->username] : []),
+            ...(isset($report->entry_reported) ? [
+                'reported' =>
+                    [
+                        'page_name' => $report->entry_reported->name,
+                        'page_description' => $report->entry_reported->description,
+                        'page_url' => $report->entry_reported->url,
+                        'entry_author' => $report->entry_reported->author->username,
+                        'is_nsfw' => $report->entry_reported->is_nsfw
+                    ],
+            ] : []),
+            'reason' => $report->reason,
+            'reported_at' => $report->created_at,
+            'last_update_at' => $report->updated_at,
+            'assigned_to' => $report->resolver,
+            'is_resolved' => $report->resolved,
+        ];
+
+        return \lib\Responsivity::respond($response);
+    }
+}
