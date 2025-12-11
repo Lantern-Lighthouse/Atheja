c639de764db5e194435c5cf9533ccb3a21b53326
Písek Pískovec <pisekpiskovec@gmail.com>
2025-12-11 22:07:08 +0100

    feat: Add new HTTP request model and endpoint for report retrieval.
    - Add new HTTP request model for retrieving reports with specified parameters.
    - Rename request to 'Get report' and update the last modified date.
    - Add new endpoint for retrieving all reports.
    - Add a new method to retrieve and filter reports based on their status with access control and proper error responses.

--- app/Controllers/Report.php
+++ app/Controllers/Report.php
@@ -14,6 +14,8 @@ class Report
 
         $model = new \Models\Report();
         $report = $model->findone(['id=?', $base->get('PARAMS.reportID')]);
+        if (!$report)
+            return \lib\Responsivity::respond('No report found', \lib\Responsivity::HTTP_No_Content);
         unset($model);
 
         $response = [
@@ -35,6 +37,62 @@ class Report
             'assigned_to' => $report->resolver,
             'is_resolved' => $report->resolved,
         ];
+
+        return \lib\Responsivity::respond($response);
+    }
+
+    public function getReports(\Base $base)
+    {
+        $rbac = \lib\RibbitCore::get_instance($base);
+        $user = VerifySessionToken($base);
+        $rbac->set_current_user($user);
+        if (!$rbac->has_role('moderator') && !$rbac->has_role('admin'))
+            return \lib\Responsivity::respond('Unauthorized', \lib\Responsivity::HTTP_Unauthorized);
+
+        $model = new \Models\Report();
+        switch ($base->get('GET.filter')) {
+            case "open":
+            default:
+                $filter = ["resolved=0"];
+                break;
+            case "closed":
+                $filter = ["resolved=1"];
+                break;
+            case "assigned":
+                $filter = ["resolved=1"];
+                break;
+            case "unassigned":
+                $filter = ["resolver=?", NULL];
+                break;
+        }
+        $reports = $model->find($filter);
+        if (!$reports)
+            return \lib\Responsivity::respond('No report found', \lib\Responsivity::HTTP_No_Content);
+        unset($model);
+        $response = [];
+
+        foreach ($reports as $report) {
+
+            $response[$report->id] = [
+                'reporter' => $report->reporter->username,
+                ...(isset($report->user_reported) ? ['reported' => $report->user_reported->username] : []),
+                ...(isset($report->entry_reported) ? [
+                    'reported' =>
+                        [
+                            'page_name' => $report->entry_reported->name,
+                            'page_description' => $report->entry_reported->description,
+                            'page_url' => $report->entry_reported->url,
+                            'entry_author' => $report->entry_reported->author->username,
+                            'is_nsfw' => $report->entry_reported->is_nsfw
+                        ],
+                ] : []),
+                'reason' => $report->reason,
+                'reported_at' => $report->created_at,
+                'last_update_at' => $report->updated_at,
+                'assigned_to' => $report->resolver,
+                'is_resolved' => $report->resolved,
+            ];
+        }
 
         return \lib\Responsivity::respond($response);
     }
